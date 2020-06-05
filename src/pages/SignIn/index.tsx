65acf08b5926bab189c286baaa3ae85a12e01a10
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2020-06-05 18:45:13 -0300

    Added context api for authentication state control

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-
+import React, { useCallback, useRef } from 'react';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/AuthContext';
+import validationErrors from '../../utils/getValidationErrors';
+
 import logo from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -8,12 +14,46 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface FormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+
+  const handleSubmit = useCallback(
+    async (data: FormData) => {
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('E-mail inválido')
+            .required('E-mail é obrigatório'),
+          password: Yup.string().required('Senha é obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { email, password } = data;
+
+        signIn({ email, password });
+      } catch (err) {
+        const errors = validationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn]
+  );
+
   return (
     <Container>
       <Content>
         <img src={logo} alt="GoBarv=barber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
           <Input
@@ -24,7 +64,7 @@ const SignIn: React.FC = () => {
           />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
         <a href="/cadastro">
           <FiLogIn />
           Criar conta
